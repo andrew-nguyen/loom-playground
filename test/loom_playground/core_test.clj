b6a01be700654ef879c76a5c3961dd644257dcab
Andrew Nguyen <andrew@klarahealth.com>
2014-05-29 21:09:21 -0700

    Initial creation of repos from lein new

---
+++ test/loom_playground/core_test.clj
@@ -0,0 +1,7 @@
+(ns loom-playground.core-test
+  (:require [clojure.test :refer :all]
+            [loom-playground.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
